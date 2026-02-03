15f31e49332720e7fc077563087da1e4f32acc69
Ishan Sasane <ishansasane999@gmail.com>
2026-02-03 16:40:35 +0530

    redux-setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,8 @@ import { Stack } from "expo-router";
 import "../global.css";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { useFonts } from "expo-font";
+import { Provider } from "react-redux";
+import { store } from "@/redux/store";
 
 export default function RootLayout() {
   const [fontLoaded] = useFonts({
@@ -9,10 +11,12 @@ export default function RootLayout() {
     nothing: require("./../assets/fonts/LEDDot-Matrix.ttf"),
   });
   return (
-    <SafeAreaProvider>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tabs)" />
-      </Stack>
-    </SafeAreaProvider>
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(tabs)" />
+        </Stack>
+      </SafeAreaProvider>
+    </Provider>
   );
 }
