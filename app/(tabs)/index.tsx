d02eaaee8d581b97278c90632fe015707e87326f
Ishan Sasane <ishansasane999@gmail.com>
2026-02-04 11:33:21 +0530

    search-bar

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,14 +4,22 @@ import { useDispatch, useSelector } from "react-redux";
 
 import Card from "../components/card";
 import { RootType } from "@/redux/store";
+import { Searchbar } from "react-native-paper";
 
 export default function Index() {
   const data = useSelector((state: RootType) => state.article.data);
   return (
-    <SafeAreaView className="flex-1 bg-black px-4">
+    <SafeAreaView className="flex-1 bg-black px-4 gap-2">
       <Text style={{ fontFamily: "nothing" }} className="text-white text-4xl">
         Articales
       </Text>
+      <Searchbar
+        mode="bar"
+        loading={true}
+        style={{ height: 50 }}
+        placeholder="Search"
+        value=""
+      ></Searchbar>
 
       <FlatList
         data={data}
